f89419e1baeadf98ef6bf741638f3c79cfc0830f
Sergio Garcia <sergio.garcia5417@gmail.com>
2026-04-30 18:47:14 -0500

    Update LINK lengths in GUI.py

--- GUI.py
+++ GUI.py
@@ -66,7 +66,7 @@ FRAME_MS = 16
 MIN_MOTION_STEPS = 35
 
 #LINKS = [base_height, link1, link2, link3, wrist/tool_offset]
-LINKS = [130, 160, 180, 40, 20]
+LINKS = [70, 80, 60, 40, 20]
 DEFAULT_ANGLE = 90
 
 WORK_MIN = {
